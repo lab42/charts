947a9350cb92edfc489a882b12a35fd0b120a201
Dany Henriquez <danyhenriquez86@gmail.com>
2022-10-14 21:04:43 +0200

    Initial build

--- gen.go
+++ gen.go
@@ -1,46 +1,73 @@
-//go:build ignore
+//go:generate go run gen.go
 
 package main
 
 import (
 	"fmt"
-	"log"
-	"os/exec"
+	"io/ioutil"
 	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/registry"
 )
 
 func main() {
-	charts, _ := filepath.Glob("./src/*")
-	for _, c := range charts {
-		if chartHasUpdates(c) {
-			cmd := exec.Command("helm", "package", "--destination", "./packages", c)
-			if err := cmd.Run(); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-}
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Info().Msg("Processing packages")
 
-type Chart struct {
-	APIVersion  string `yaml:"apiVersion"`
-	Name        string `yaml:"name"`
-	Description string `yaml:"description"`
-	Type        string `yaml:"type"`
-	Version     string `yaml:"version"`
-}
+	helmCharts, _ := filepath.Glob("./src/*")
+	linter := action.NewLint()
+	log.Info().Msg("Linting")
+	result := linter.Run(helmCharts, make(map[string]interface{}))
 
-func chartHasUpdates(path string) bool {
-	cmd := exec.Command("git", "diff", "--exit-code", "HEAD^", path)
-
-	if err := cmd.Start(); err != nil {
-		log.Fatalf("cmd.Start: %v", err)
-	}
-
-	if err := cmd.Wait(); err != nil {
-		if _, ok := err.(*exec.ExitError); ok {
-			return true
+	for _, err := range result.Errors {
+		if err != nil {
+			log.Error().Err(err)
 		}
 	}
 
-	return false
+	for _, helmChart := range helmCharts {
+		pkg := action.NewPackage()
+		pkg.Destination = "./packages"
+
+		if _, err := pkg.Run(helmChart, make(map[string]interface{})); err != nil {
+			log.Error().Err(err)
+		}
+		log.Info().Msg("Packaged: " + helmChart)
+	}
+
+	var loginOptions []registry.LoginOption
+	loginOptions = append(loginOptions, registry.LoginOptInsecure(true))
+	loginOptions = append(loginOptions, registry.LoginOptBasicAuth("DanyHenriquez", "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"))
+
+	client, err := registry.NewClient()
+	if err != nil {
+		log.Error().Err(err)
+	}
+	client.Login("ghcr.io/lab42/charts", loginOptions...)
+
+	versionPattern := regexp.MustCompile(`([0-9]+\.[0-9]+\.[0-9]+)`)
+	helmPackages, _ := filepath.Glob("./packages/*.tgz")
+
+	for _, helmPackage := range helmPackages {
+		b, err := ioutil.ReadFile(helmPackage)
+		if err != nil {
+			log.Error().Err(err)
+		}
+
+		helmPackageVersion := versionPattern.FindString(helmPackage)
+		helmPackageName := strings.TrimSuffix(helmPackage, fmt.Sprintf("-%s.tgz", helmPackageVersion))
+
+		info, err := client.Push(b, fmt.Sprintf("ghcr.io/lab42/charts/%s:%s", strings.TrimPrefix(helmPackageName, "packages/"), helmPackageVersion))
+		if err != nil {
+			log.Error().Err(err)
+		}
+
+		log.Info().Msg("Pushed: " + info.Ref)
+		log.Info().Msg("Digest: " + info.Manifest.Digest)
+	}
 }
