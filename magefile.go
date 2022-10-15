06e3c2d45f9feb4d5683afc74ed300b163cdd86d
Dany Henriquez <danyhenriquez86@gmail.com>
2022-10-16 00:20:33 +0200

    Back to OCI

--- magefile.go
+++ magefile.go
@@ -5,18 +5,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v2"
 	"helm.sh/helm/v3/pkg/action"
-	"helm.sh/helm/v3/pkg/repo"
+	"helm.sh/helm/v3/pkg/registry"
 	// mg contains helpful utility functions, like Deps
 )
 
@@ -37,94 +34,100 @@ func Lint() error {
 	return nil
 }
 
-// Update Helm registry
-func Update() error {
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+type HelmChart struct {
+	APIVersion  string `yaml:"apiVersion"`
+	Name        string `yaml:"name"`
+	Description string `yaml:"description"`
+	Type        string `yaml:"type"`
+	Version     string `yaml:"version"`
+}
 
-	log.Info().Msg("Cloning registry")
-	url := fmt.Sprintf("https://%s:%s@github.com/lab42/registry.git", os.Getenv("USERNAME"), os.Getenv("TOKEN"))
-	repository, err := git.PlainClone("./charts", false, &git.CloneOptions{
-		URL:           url,
-		ReferenceName: plumbing.ReferenceName("refs/heads/main"),
-		SingleBranch:  true,
-	})
-
-	if err != nil {
-		return err
+func newClient() (*registry.Client, error) {
+	loginOptions := []registry.LoginOption{
+		registry.LoginOptBasicAuth(os.Getenv("USERNAME"), os.Getenv("TOKEN")),
 	}
 
-	helmCharts, err := filepath.Glob("./src/*")
+	client, err := registry.NewClient()
 	if err != nil {
-		return err
+		return client, err
+	}
+
+	return client, client.Login("ghcr.io/lab42/charts", loginOptions...)
+}
+
+// Return Helm charts info
+func chartInfo(glob string) ([]HelmChart, error) {
+	out := []HelmChart{}
+
+	helmCharts, err := filepath.Glob(glob)
+	if err != nil {
+		return out, err
 	}
 
 	for _, helmChart := range helmCharts {
+		b, err := ioutil.ReadFile(
+			fmt.Sprintf("%s/Chart.yaml", helmChart),
+		)
+		if err != nil {
+			return out, err
+		}
+
+		var helmChartYaml HelmChart
+		err = yaml.Unmarshal(b, &helmChartYaml)
+		if err != nil {
+			return out, err
+		}
+
+		out = append(out, helmChartYaml)
+	}
+
+	return out, nil
+}
+
+func Package() error {
+	helmCharts, err := chartInfo("./src/*")
+	if err != nil {
+		return err
+	}
+
+	for _, helmChartInfo := range helmCharts {
 		pkg := action.NewPackage()
 		pkg.Destination = "./charts"
 
-		if _, err := pkg.Run(helmChart, make(map[string]interface{})); err != nil {
+		if _, err := pkg.Run(fmt.Sprintf("./src/%s", helmChartInfo.Name), make(map[string]interface{})); err != nil {
 			return err
 		}
-		log.Info().Msg("Packaged: " + helmChart)
+		log.Info().Msg(fmt.Sprintf("Packaged: %s %s", helmChartInfo.Name, helmChartInfo.Version))
 	}
 
-	if err := index(url); err != nil {
-		return err
-	}
-
-	workTree, err := repository.Worktree()
-	workTree.AddGlob("*.tgz")
-	workTree.AddGlob("*.yaml")
-
-	hash, err := workTree.Commit(os.Getenv("GITHUB_SHA"),
-		&git.CommitOptions{
-			All: true,
-			Author: &object.Signature{
-				Name:  "Dany Henriquez",
-				Email: "danyhenriquez86@gmail.com",
-				When:  time.Now(),
-			},
-		},
-	)
-	if err != nil {
-		return nil
-	}
-
-	log.Info().Msg("Commit hash: " + hash.String())
-	log.Info().Msg("Pushing registry")
-
-	return repository.Push(&git.PushOptions{})
+	return nil
 }
 
-func index(url string) error {
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
-
-	out := filepath.Join("charts", "index.yaml")
-	mergeTo := ""
-
-	if _, err := os.Stat(out); err == nil {
-		mergeTo = out
-	}
-
-	i, err := repo.IndexDirectory("charts", url)
+func Push() error {
+	client, err := newClient()
 	if err != nil {
 		return err
 	}
 
-	if mergeTo != "" {
-		// if index.yaml is missing then create an empty one to merge into
-		var i2 *repo.IndexFile
-		if _, err := os.Stat(mergeTo); os.IsNotExist(err) {
-			i2 = repo.NewIndexFile()
-			i2.WriteFile(mergeTo, 0644)
-		} else {
-			i2, err = repo.LoadIndexFile(mergeTo)
-			if err != nil {
-				return errors.Wrap(err, "merge failed")
-			}
-		}
-		i.Merge(i2)
+	helmCharts, err := chartInfo("./src/*")
+	if err != nil {
+		return err
 	}
-	i.SortEntries()
-	return i.WriteFile(out, 0644)
+
+	for _, helmChartInfo := range helmCharts {
+		b, err := ioutil.ReadFile(fmt.Sprintf("./charts/%s-%s.tgz", helmChartInfo.Name, helmChartInfo.Version))
+		if err != nil {
+			log.Error().Err(err)
+		}
+
+		info, err := client.Push(b, fmt.Sprintf("ghcr.io/lab42/charts/%s:%s", helmChartInfo.Name, helmChartInfo.Version))
+		if err != nil {
+			log.Error().Err(err)
+		}
+
+		log.Info().Msg("Pushed: " + info.Ref)
+		log.Info().Msg("Digest: " + info.Manifest.Digest)
+	}
+
+	return nil
 }
